c959d1350859eda3c90e98719a7e95dfc4ed9e4c
CreepyCrafter24 <creepycrafter24.devel@gmail.com>
2018-12-23 14:40:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-self.addEventListener('oninstall', event => { Install(); });
-self.addEventListener('install', event => { Install(); });
-self.addEventListener('fetch', event => { Fetch(); });
-function Install() {
-    console.log('install');
+const staticAssets = ['./'];
+self.addEventListener('oninstall', function (event) { Install(); });
+self.addEventListener('install', function(event) { Install(); });
+self.addEventListener('fetch', function (event) { Fetch(); });
+async function Install() {
+    console.log('installing...');
+    const cache = await caches.open('Netzgottesdienst-static');
+    cache.addAll(staticAssets);
 }
 
 function Fetch() {
-    console.log('fetch');
+    console.log('fetching...');
 }
